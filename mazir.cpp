260d739c6b8ecb4282d7f22ae6276b0774e10ba3
niskakenzhebek91-jpg <niskakenzhebek91@gmail.com>
2026-02-26 02:11:41 +0500

    МӘЗІР.cpp

---
+++ mazir.cpp
@@ -0,0 +1,9 @@
+void show_menu() {
+    print_separator();
+    cout << "  БАСТЫ МӘЗІР" << endl;
+    print_separator();
+    cout << "  1 – Тесті бастау (9 сұрақ)" << endl;
+    cout << "  2 – Тақырыптар бойынша анықтама" << endl;
+    cout << "  0 – Шығу" << endl;
+    print_separator();
+}
